828e83f0fdbf1501c792f82d33ab48fc5cbfa4ea
Casey Smith <34159052+Casey6624@users.noreply.github.com>
2018-06-05 17:30:16 +0100

    Final Submit
    Small project, used to practice JavaScript in Chrome Extensions. Picks a random image from the image store

---
+++ hi.js
@@ -0,0 +1,16 @@
+filenames = [
+  "hi1.png",
+  "hi2.png",
+  "hi3.png",
+  "hi4.png"
+]
+
+let imgs = document.getElementsByTagName('img');
+
+for (imgElt of imgs){
+  let r = Math.floor(Math.random() * filenames.length);
+  let file = "img/" + filenames[r];
+  let url = chrome.extension.getURL(file);
+  imgElt.src = url;
+  console.log(url);
+}
